16873558d975881e11f8c7cd282ddd838a6f4f7a
rayennradhouani <rayenradhouani5@gmail.com>
2025-08-20 13:25:41 +0100

    Setup GitHub Actions workflow pour déploiement

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/mon-portfolio/'  // obligatoire pour GitHub Pages
+  base: '/mon-portfolio/'   // IMPORTANT pour GitHub Pages
 })
